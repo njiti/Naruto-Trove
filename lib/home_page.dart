f9e4a4bfcf3d72092e72e9fdbc23b9f9670a5def
njiti <kamaukan77@gmail.com>
2024-05-31 23:22:17 +0300

    simplified code by making a class for the widgets and only having to call them at home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:naruto_trove/model.dart';
+import 'package:naruto_trove/normal_card.dart';
 
 import 'api_handler.dart';
 
@@ -34,38 +35,38 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.teal,
         foregroundColor: Colors.white,
       ),
-      bottomNavigationBar: MaterialButton(
-        color: Colors.teal,
-        textColor: Colors.white,
-        padding: const EdgeInsets.all(20),
-        onPressed: () {
-          getData();
-          print(data.length);
-        },
-        child: const Text('Refresh'),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: 1,
-            backgroundColor: Colors.teal,
-            foregroundColor: Colors.white,
-            onPressed: () {},
-            child: const Icon(Icons.search),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            heroTag: 2,
-            backgroundColor: Colors.teal,
-            foregroundColor: Colors.white,
-            onPressed: () {},
-            child: const Icon(Icons.add),
-          )
-        ],
-      ),
+      // bottomNavigationBar: MaterialButton(
+      //   color: Colors.teal,
+      //   textColor: Colors.white,
+      //   padding: const EdgeInsets.all(20),
+      //   onPressed: () {
+      //     getData();
+      //     print(data.length);
+      //   },
+      //   child: const Text('Refresh'),
+      // ),
+      // floatingActionButton: Column(
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   children: [
+      //     FloatingActionButton(
+      //       heroTag: 1,
+      //       backgroundColor: Colors.teal,
+      //       foregroundColor: Colors.white,
+      //       onPressed: () {},
+      //       child: const Icon(Icons.search),
+      //     ),
+      //     const SizedBox(
+      //       height: 10,
+      //     ),
+      //     FloatingActionButton(
+      //       heroTag: 2,
+      //       backgroundColor: Colors.teal,
+      //       foregroundColor: Colors.white,
+      //       onPressed: () {},
+      //       child: const Icon(Icons.add),
+      //     )
+      //   ],
+      // ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -74,16 +75,29 @@ class _HomePageState extends State<HomePage> {
               shrinkWrap: true,
               itemCount: data.length,
               itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  onTap: () {},
-                  leading: Text(data[index].name),
-                  // title: Text("${data[index].total}"),
-                  // subtitle: Text("${data[index].total}"),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete_outline),
-                    onPressed: () {},
-                  ),
+                return MySquare(
+                  image: '${data[index].images.firstOrNull}',
+                  Name: '${data[index].name}',
+                  Education: '${data[index].family?.mother}',
+                  Qualifications: '${data[index].family?.father}',
+
                 );
+                // return ListTile(
+                //   onTap: () {},
+                //   leading: Text("${data[index].id}"),
+                //   title: Text("${data[index].name}"),
+                //   subtitle: Image.network(
+                //     "${data[index].images.firstOrNull}",
+                //     height: 90,
+                //     width: 100,
+                //   ),
+                //   trailing: Text("${data[index].family?.mother}"),
+                //   // subtitle: Text("${data[index].total}"),
+                //   // trailing: IconButton(
+                //   //   icon: const Icon(Icons.delete_outline),
+                //   //   onPressed: () {},
+                //   // ),
+                // );
               },
             )
           ],
